138b90f33bf36fece2f26ad9ae1b5907c6690d93
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-12-05 16:22:37 +0900

    ts + form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="red" bgColor="teal" />
-      <Circle bgColor="tomato" text="im here" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
